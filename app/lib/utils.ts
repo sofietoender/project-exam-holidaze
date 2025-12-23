f9bb8ad1c778239c710dfabc8ce12eed73dbedcd
Sofie Tønder <sofie.ts@hotmail.com>
2025-12-23 23:02:27 +0100

    Update utils.ts

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,4 +1,4 @@
-import { clsx, type ClassValue } from "clsx"
+npm run devimport { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
