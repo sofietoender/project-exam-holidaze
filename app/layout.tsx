677e6e89f8f286c192ffe610a767870b9759ce0e
Sofie Tønder <sofie.ts@hotmail.com>
2025-12-23 22:49:53 +0100

    add loginform

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const outfit = Outfit({
 	variable: "--font-outfit",
@@ -19,7 +20,10 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en" suppressHydrationWarning>
-			<body className={`${outfit.variable} antialiased`}>{children}</body>
+			<body className={`${outfit.variable} antialiased`}>
+				{children}
+				<Toaster />
+			</body>
 		</html>
 	);
 }
