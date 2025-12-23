9f7753122efb839798fd348cedc088a1f029d03b
Sofie Tønder <sofie.ts@hotmail.com>
2025-12-23 06:06:36 +0100

    added header component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,8 @@
 import Link from "next/link";
-import AboutHero from "../components/AboutHero";
 
 export default function About() {
 	return (
 		<div>
-			<AboutHero />
 			<Link href="/">Hjem</Link>
 		</div>
 	);
