3de676e91d58c20a7ce86fce6535034ae4dffd12
Sofie Tønder <sofie.ts@hotmail.com>
2025-12-29 19:31:36 +0100

    add search component to venue page

--- app/(main)/venues/page.tsx
+++ app/(main)/venues/page.tsx
@@ -1,13 +1,20 @@
-import Link from "next/link";
+import SearchBar from "@/components/search/SearchBar";
 
-export default function Venues() {
+export default function VenuesPage() {
 	return (
-		<div>
-			<h1>All Venues page</h1>
-			<ul>
-				<li>Link to venue with id</li>
-			</ul>
-			<Link href="/">Hjem</Link>
+		<div className="container mx-auto px-4 py-8 md:py-12">
+			{/* Header */}
+			<div className="mb-8">
+				<h1 className="mb-2 text-3xl font-bold md:text-4xl">Results for ...</h1>
+				<p className="text-muted-foreground"> Found 10 venue</p>
+			</div>
+
+			{/* Search */}
+			<div className="mb-8 max-w-md">
+				<SearchBar />
+			</div>
+
+			{/* Venues Grid */}
 		</div>
 	);
 }
